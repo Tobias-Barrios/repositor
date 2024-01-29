7d3b328fdd58367a38e0ce8d27fbf80a91a296b0
Tobias-Barrios <tobias_fabi30hotmail.com>
2024-01-29 19:56:58 -0300

    locomnotora

--- class.py
+++ class.py
@@ -1,9 +1,9 @@
 print ("Hola giles")
 v1 = int (input())
+v2 = int (input())
 if v1 > 3: print ("imbecil co so vo")
-
-
-
+v3 =  float(input())
+"voy a todo motor porque soy una locomotora ~ Luis miguel"
 
 
 
